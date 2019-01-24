c5262049e93d6cfee0b62d88f777ab2655437c3c
Ablova Yuliya <y.ablova@qiwi.com>
2019-01-25 00:14:34 +0300

    init db with random by ffaker

--- blog/db/seeds.rb
+++ blog/db/seeds.rb
@@ -1,41 +1,46 @@
-def random_string(min_length, max_length, simple)
-  charset = [*('a'..'z'),*('0'..'9'),*('A'..'Z')]
-  unless simple
-    charset += [*[' ']*10,*['. ']*3]
-  end
+Comment.destroy_all
+Mark.destroy_all
+Post.destroy_all
+User.destroy_all
 
-  return (1 .. rand(min_length..max_length)).map{charset[rand(charset.length)]}.join
+users_hash = 10.times.map do {
+  name: FFaker::Internet.user_name,
+  email: FFaker::Internet.safe_email,
+  creator: (rand(3) < 2)
+  }
 end
 
-10.times do |i|
-  name = random_string(2, 20, true)
-  is_creator = (rand(3) < 2)
-  u = User.create(name: name, email: "#{name}@email.com", creator: is_creator)
-  
-  if is_creator
-    # it could be zero times
-    1..rand(5).times do |i|
-      p = Post.create(user: u, title: "Blog post ##{i}", body: random_string(1, 350, false))
-    end
+users = User.create!(users_hash)
+
+posts_hash = users.select{|user| user.creator}.map do |user|
+  rand(5).times.map do {
+    user: user,
+    title: FFaker::Book.title,
+    body: FFaker::Book.description
+  }
   end
 end
 
-users = User.all
-Post.all.each do |p|
-  # has marks? 66%
-  if rand(3) < 2
-    users.sample(rand(1..5)).each do |u|
-      # don't mark your own posts
-      if p.user != u
-        Mark.create(post: p, user: u, mark: rand(3..5))
-      end
-    end
-  end
+posts = Post.create!(posts_hash.flatten)
 
-  # has comments? 50%
-  if rand(2) == 0
-    users.sample(rand(1..3)).each do |u|
-      Comment.create(post: p, user: u, body: random_string(1, 120, false))
-    end
+mark_hash = posts.select{|post| rand(3)<2}.map do |post|
+  users.sample(rand(1..5)).select{|user| post.user != user}.map do |user| {
+    post: post,
+    user: user,
+    mark: rand(3..5)
+  }
   end
 end
+
+Mark.create!(mark_hash.flatten)
+
+comment_hash = posts.select{|post| rand(2)==0}.map do |post|
+  users.sample(rand(1..3)).map do |user| {
+    post: post,
+    user: user,
+    body: FFaker::Tweet.body
+  }
+  end
+end
+
+Comment.create!(comment_hash.flatten)
