1c4e871935bf53e2de583324cda63a54a4f180db
Ablova Yuliya <y.ablova@qiwi.com>
2019-01-25 02:45:02 +0300

    users: post counter

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, counter_cache: true
 
   has_many :comments, as: :commentable, dependent: :destroy
   has_many :marks, dependent: :destroy
