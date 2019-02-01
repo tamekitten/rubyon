46baa7f94ab6360399b6ff8ca5c9a0f47ee89c95
Ablova Yuliya <y.ablova@qiwi.com>
2019-02-01 20:28:27 +0300

    switch locale

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :users
-  resources :posts
-  root 'home#index'
+  scope "(:locale)", locale:/#{I18n.available_locales.join("|")}/ do
+    devise_for :users
+    resources :users
+    resources :posts
+    root 'home#index'
 
-  get 'about_me', to: 'home#about_me'
-  get '/home/(:hello)', to: 'home#index'
+    get 'about_me', to: 'home#about_me'
+    get '/home/(:hello)', to: 'home#index'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
